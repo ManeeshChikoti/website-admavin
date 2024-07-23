c493fa856982669485ea42b16a72442b30c5bbed
ManeeshChikoti <maneeshchikoti@gmail.com>
2024-07-23 11:03:29 +0530

    added home page

--- scripts/why-admavin/why-admavin-functions.js
+++ scripts/why-admavin/why-admavin-functions.js
@@ -14,63 +14,50 @@ function showMessage(clickedButton, number) {
       messageHeading.innerText = "TRUSTED DATA SOURCES";
       messageText.innerText =
         "Get - “Up to Date” location specific data and analytics for more than 700 cities and towns across India, including real time Traffic, Demographics and Population spread data.";
-        
+      messageDiv.style.backgroundImage = 'url("../../assets/1.png")';
       break;
 
     case 2:
       messageHeading.innerText = "ROBUST VERIFIED ALGORITHMS";
       messageText.innerText =
         "We constantly enhance our Algorithms and verify them through our collaboration with India's premier Technology Institutions like IIT Madras and other Industry sources.";
+      messageDiv.style.backgroundImage = 'url("../../assets/2.png")';
+
       break;
     case 3:
       messageHeading.innerText = "END TO END WORKFLOWS";
       messageText.innerText =
         "Proprietary or Confidential data? You can upload your media and data layers in your own secure server under a white-labeled Application.";
+      messageDiv.style.backgroundImage = 'url("../../assets/3.png")';
+
       break;
 
     case 4:
       messageHeading.innerText = "MAPS IMAGES & SCORECARDS";
       messageText.innerText =
         "Clients love visuals. Our Tools provide automated and customizable visualization for your presentations and reports.";
+      messageDiv.style.backgroundImage = 'url("../../assets/4.png")';
+
       break;
 
     case 5:
       messageHeading.innerText = "END TO END WORKFLOWS";
       messageText.innerText =
         "Analyze Geographies - Plan OOH Campaigns - Contact Media Owners - Setup and Monitor Campaigns. All on a Single Platform.";
+      messageDiv.style.backgroundImage = 'url("../../assets/5.png")';
+
       break;
 
     case 6:
       messageHeading.innerText = "SUPPORT";
       messageText.innerText =
         "Learning to adapt, to data driven media markets? We support you in this journey and walk you through the data.";
+      messageDiv.style.backgroundImage = 'url("../../assets/6.png")';
+
       break;
   }
 
-  // canvas element for creating background Imgae
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-
-  // setting canvas styles
-  canvas.width = 200;
-  canvas.height = 200;
-  ctx.font = "100px kanti";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillStyle = "#00c09b";
-
-  // Clear canvas,to remove any  any previous content
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the chosen number on the canvas
-  ctx.fillText(number.toString(), canvas.width / 2, canvas.height / 2);
-
-  // Set chosen number as background image text
-
-  const imgData = ["", "", ""]; 
- 
-  messageDiv.style.backgroundImage = `<img src="${imgData[number-1]}"/>`;
   messageDiv.style.backgroundRepeat = "no-repeat";
-  messageDiv.style.backgroundSize = "cover";
+  messageDiv.style.backgroundSize = "contain";
   messageDiv.style.backgroundPosition = "center";
 }
